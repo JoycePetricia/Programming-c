ee98eb439044f7888afcf042c850dca344538bbd
JoycePetricia <joycepetricia29@gmail.com>
2025-02-05 13:21:03 +0530

    Create basic.c

---
+++ basic.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int a = 10;
+    float b = 3.14;
+    char c = 'X';
+    printf("Integer: %d\n", a);
+    printf("Float: %.2f\n", b);
+    printf("Character: %c\n", c);
+    return 0;
+}
