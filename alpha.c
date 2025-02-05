0f61011f92dea347cbc916939086656b3d615e7e
JoycePetricia <joycepetricia29@gmail.com>
2025-02-05 15:39:36 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+     char ch;
+     printf("Enter the character:");
+     scanf("%c" ,&ch);
+     printf("The ascii of the char %c is %d ",ch,ch);
+    return 0;
+}
