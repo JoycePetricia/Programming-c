6469da2d7d4676dd0952a1d61b662f16131d07d3
JoycePetricia <joycepetricia29@gmail.com>
2025-02-05 15:18:05 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+     int num1,num2,num3;
+     float average;
+     num1=12;
+     num2=15;
+     num3=19;
+     average=(float)(num1+num2+num3);
+     printf("average=%f",average);
+    return 0;
+}
